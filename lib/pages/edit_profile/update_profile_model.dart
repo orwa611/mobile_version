787152de81d2b2ec7caae773b506c8c36a57a191
ouroua kaddouri <ouroua.kaddouri@gmail.com>
2025-12-06 18:31:06 +0100

    create update profile model

---
+++ lib/pages/edit_profile/update_profile_model.dart
@@ -0,0 +1,13 @@
+final class UpdateProfileModel {
+  final String firstName;
+  final String lastName;
+  final String email;
+  final String bio;
+
+  UpdateProfileModel({
+    required this.firstName,
+    required this.lastName,
+    required this.email,
+    required this.bio,
+  });
+}
